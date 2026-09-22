38b3a1646e7bf09b6435b72a29d21df3e1f87186
wafaagomaa <wafaagomaa2025@gmail.com>
2026-09-22 20:46:13 +0300

    Initial commit

---
+++ story_content/6m92cHHYVHn_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":39,"text":"مبروك كده أنهيت رحلة التصميم الهندسي"},{"start":3200,"text":"من تحديد المشكلة لحد تطوير الحل ومشاركته"},{"start":6880,"text":"افتكر دايما إن كل ابتكار عظيم"},{"start":9319,"text":"بدأ بفكرة وتطور بالبحث والتجربة والتحسين المستمر"},{"start":14279,"text":"خليك دايما فضولي واسأل وابتكر حلول تقدر تعمل بيها فرق حقيقي."}]}]};
+    window.globalLoadJsAsset('story_content/6m92cHHYVHn_transcripts.js', JSON.stringify(data));
+})();
